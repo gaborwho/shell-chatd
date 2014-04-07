c32bb71ad5c235518f2856f61afe7394672a0206
Gabor Balla <cavalierrant@gmail.com>
2014-04-07 13:38:46 +0200

    second commit

--- html.sh
+++ html.sh
@@ -2,7 +2,7 @@ hidden () {
 	echo -n "<input type=\"hidden\" name=\"$name\" value=\"$value\" />"
 }
 input () {
-	echo -n "<input type=\"text\" name=\"$name\" />"
+	echo -n "<input type=\"text\" name=\"$name\" autofocus />"
 }
 submit () {
 	echo -n "<input type=\"submit\" />"
