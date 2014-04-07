c1a3cb2e78d000865243b8852c25fb7ebf86e44e
Gabor Balla <cavalierrant@gmail.com>
2014-04-07 23:51:53 +0200

    template system

--- html.sh
+++
@@ -1,9 +0,0 @@
-hidden () {
-	echo -n "<input type=\"hidden\" name=\"$name\" value=\"$value\" />"
-}
-input () {
-	echo -n "<input type=\"text\" name=\"$name\" autofocus />"
-}
-submit () {
-	echo -n "<input type=\"submit\" />"
-}
